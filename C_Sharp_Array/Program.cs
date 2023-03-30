fba77ac223ae53a782c3a0263406046ed1afcb9f
Oleg Streltsov <olegstrelsov2003@gmail.com>
2023-03-31 00:50:32 +0300

    Add project files.

---
+++ C_Sharp_Array/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Text;
+
+namespace Array_Multidimensional_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+            Console.WriteLine("Завдання 1");
+            Console.Write("Введіть розмір масиву: ");
+            int size = int.Parse(Console.ReadLine());
+            int[] array = new int[size];
+            Random rnd = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = rnd.Next(0, 10);
+            }
+            foreach (var item in array)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+            Array.Sort(array);
+            Console.WriteLine($"Мінімалтне значення в масиві: {array[0]}\nМаксимальне значення в масиві: {array[size - 1]}");
+            Console.WriteLine("Завдання 2");
+            int num = array[0];
+            bool found = false;
+            for (int i = 1; i < size; i++)
+            {
+                if (num == array[i])
+                {
+                    found = true;
+                    break;
+                }
+                num = array[i];
+            }
+            Console.WriteLine($"Результат: {(found ? "У масиві є однакові елементи" : "У масиві немає однакових елементів")}");
+            Console.WriteLine("Завдання 3");
+            Console.Write("Введіть розмір масиву: ");
+            int size1 = int.Parse(Console.ReadLine());
+            int[] array1 = new int[size1];
+            for (int i = 0; i < size1; i++)
+            {
+                array1[i] = rnd.Next(0, 10);
+            }
+            foreach (var item in array1)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+            int count = 0;
+            double avg = 0;
+            for (int i = 0; i < size1; i++)
+            {
+                if ((i % 2) == 0 && (array1[i] % 2) == 0)
+                {
+                    count++;
+                    avg += array1[i];
+                }
+            }
+            Console.WriteLine($"Середнє арефметичне значення: {avg / count}");
+            Console.WriteLine("Завдання 4");
+            Console.Write("Введіть розмір масиву: ");
+            int size2 = int.Parse(Console.ReadLine());
+            int[] array2 = new int[size2];
+            for (int i = 0; i < size2; i++)
+            {
+                array2[i] = rnd.Next(-10, 10);
+            }
+            foreach (var item in array2)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+            count = 0;
+            int[] new_array = new int[0];
+            foreach (var item in array2)
+            {
+                if ((item % 2) == -1)
+                {
+                    count++;
+                    Array.Resize(ref new_array, count);
+                    Array.Fill(new_array, item, count - 1, 1);
+                }
+            }
+            Console.Write("Непарні значення менші нуля: ");
+            foreach (var item in new_array)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Завдання 5");
+            Console.Write("Введіть розмір масиву: ");
+            int size_all = int.Parse(Console.ReadLine());
+            int[] array_a = new int[size_all];
+            int[] array_b = new int[size_all];
+            int[] array_c = new int[size_all];
+            for (int i = 0; i < size_all; i++)
+            {
+                array_a[i] = rnd.Next(10, 30);
+                array_b[i] = rnd.Next(10, 30);
+            }
+            for (int i = 0; i < size_all; i++)
+            {
+                array_c[i] = (array_a[i] + array_b[i]);
+            }
+            Console.Write("Масив А: ");
+            foreach (var item in array_a)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.Write("\nМасив Б: ");
+            foreach (var item in array_b)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.Write("\nМасив В: ");
+            foreach (var item in array_c)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
---
+++ C_sharps/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter radius: ");
+            double radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"\t\tTask 1\nArea = {Math.PI * Math.Pow(radius, 2)}"); // first task
+            Console.WriteLine("\t\tTask 2");//second task
+            Random rnd = new Random();
+            int random = rnd.Next(0, 10);
+            int number;
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Enter number: ");
+                number = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Result: {(number == random ? "You guessed number" : "You didn't guess number")}");
+            }
+            Console.WriteLine("\t\tTask 3");//third task
+            Console.Write("Enter height: ");
+            int height = int.Parse(Console.ReadLine());
+            Console.Write("Enter length: ");
+            int length = int.Parse(Console.ReadLine());
+            Console.Write("Enter coordinate X: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Enter coordinate Y: ");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("\t\tPress any key...");
+            Console.ReadKey();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(x - 1, y - 1);
+            for (int j = 0; j < length; j++)
+            {
+                Console.Write("*");
+            }
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            for (int i = 0; i < height - 2; i++)
+            {
+                Console.SetCursorPosition(x - 1, y + i);
+                Console.WriteLine("*");
+            }
+            Console.ForegroundColor = ConsoleColor.Blue;
+            for (int i = 0; i < height - 2; i++)
+            {
+                Console.SetCursorPosition(x - 2 + length, y + i);
+                Console.WriteLine("*");
+            }
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.SetCursorPosition(x - 1, y - 2 + height);
+            for (int j = 0; j < length; j++)
+            {
+                Console.Write("*");
+            }
+            Console.ResetColor();
+            Console.WriteLine("\t\tPress any key...");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("\t\tTask 4");//fourth task
+            Console.Write("Enter number of employees: ");
+            int N = int.Parse(Console.ReadLine());
+            Console.Write("Enter working hours of the first employee: ");
+            int m = int.Parse(Console.ReadLine());
+            int FullTime = 0;
+            for (int i = 0; i < N; i++)
+            {
+                FullTime += m * 60 + i * 10;
+            }
+            Console.WriteLine($"Whole team worked for {FullTime / 60} hours and {FullTime % 60} minutes");
+            Console.WriteLine("\t\tTask 5");//fifth task
+            int num = 0;
+            do
+            {
+                Console.Write("Enter number of triangles (1 - 6): ");
+                num = int.Parse(Console.ReadLine());
+            } while (num < 1 || num > 6);
+            for (int i = 0; i < num; i++)
+            {
+                for (int k = 0; k < num; k++)
+                {
+                    for (int j = 0; j < num - 1 - i; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int j = 0; j < 2 * (i + 1) - 1; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    for (int j = 0; j < num - 1 - i; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
